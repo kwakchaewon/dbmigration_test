5bf6237c71845f5a6daababf6644a00890f117e8
kwakchaewon <cwkwak@edentns.com>
2024-05-13 10:40:36 +0900

    Initial commit

---
+++ src/main/java/com/example/dbmigration_test/DbmigrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbmigration_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmigrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmigrationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbmigration_test/DbmigrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbmigration_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmigrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
